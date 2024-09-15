b3331b4755162730cb0a1233056a1133fb60d9c5
yatinmanuel <yatin@halvex.net>
2024-09-15 03:42:12 +0100

    Cant stop

--- packages/star-ai-model/camera.py
+++ packages/star-ai-model/camera.py
@@ -14,19 +14,10 @@ cap.set(3, 640)
 cap.set(4, 480)
 
 # Load the YOLO model
-model = YOLO('yolo-weights/yolov8n.pt')
+model = YOLO('runs/train3/weights/best.pt')
 
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush", "eyeballs", "eyebrows", "eyes", "face", "facial hair"
-              ]
+classNames = ["0", "1", "2", "3", "4", "5"]
+
 # AI Logic
 while True:
     ret, image = cap.read()
